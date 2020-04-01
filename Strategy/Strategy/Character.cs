927b0e1de1babcdd9ee0423445bf987cad6f79d3
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-04-01 14:56:55 -0700

    Added Characters
    Not done guitars/solos yet

---
+++ Strategy/Strategy/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    interface Character
+    {
+        Guitar Guitar { get; set; }
+        Solo Solo { get; set; }
+
+        string getName();
+    }
+}
---
+++ Strategy/Strategy/Jimi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Jimi : Character
+    {
+        public Guitar Guitar { get; set; }
+        public Solo Solo { get; set; }
+
+        public string getName()
+        {
+            return "Jimi Hendrix";
+        }
+    }
+}
